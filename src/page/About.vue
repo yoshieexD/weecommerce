e37eb839b71861ad7d790e97c5e5ab9ea073cfb8
Jom Winston Magbag <bjomwinston0107@gmail.com>
2024-04-08 17:15:51 +0800

    :bug: fix accordion

--- src/page/About.vue
+++ src/page/About.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import Header from '@/components/layout/Header.vue';
-import Accordion from '@/components/about/Accordion.vue';
+import Accordions from '@/components/about/Accordions.vue';
 </script>
 <template>
   <Header />
-  <Accordion />
+  <Accordions />
 </template>
